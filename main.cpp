599077f53e6760abe90d3b94ed897874280287ab
Taki <tarek.laib24@gmail.com>
2026-06-18 22:46:40 +0100

    Another fatherless child

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(void){
+    std::cout << "Default Branch Name is Master, note that i am not a racist, this choice is just to demenostrat superior skill level" << std::endl;
+}
