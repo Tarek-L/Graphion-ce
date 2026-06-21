32a8f8b129b3b823ac66ac00311847dfd9012a59
Taki <tarek.laib24@gmail.com>
2026-06-21 14:48:13 +0100

    feat: prototyping window module, will finish this basic stuff later

--- src/Core/core.hpp
+++ src/Core/core.hpp
@@ -3,27 +3,20 @@
 
 #define CONCAT(x, y) x##y
 
-template<typename F>
-struct Defer {
-    F f;
-    ~Defer() { f(); }
+template <typename F> struct Defer {
+  F f;
+  ~Defer() { f(); }
 };
 
-template<typename F>
-Defer<F> make_defer(F f) {
-    return {f};
-}
+template <typename F> Defer<F> make_defer(F f) { return {f}; }
 
-#define defer(code) auto CONCAT(_defer_, __LINE__) = make_defer([&](){ code; })
+#define defer(code) auto CONCAT(_defer_, __LINE__) = make_defer([&]() { code; })
 
 template <class T> struct Error {
   T value;
   fmt::string_view message;
+  void logError() { fmt::print("[ERROR] : {}\n", message); }
 };
 
 template <class T, class E> using $ = std::expected<T, E>;
-
-template <class E> void logError(const Error<E> &err) {
-    fmt::print("[ERROR] : {}\n", err.message);
-}
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,25 @@
 #include <glad/glad.h>
 //
-#include <GLFW/glfw3.h>
 #include <Core/core.hpp>
+#include <GLFW/glfw3.h>
+#include <memory>
 
 enum class WindowError { InitializationFailed, WindowCreationFailed };
 
-class Window {
-public:
-  $<GLFWwindow*, Error<WindowError>> create();
-};
+namespace Graphion::Window {
+using WindowHandle =
+    std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)>;
 
-$<GLFWwindow*, Error<WindowError>> Window::create() {
-    GLFWwindow* window = glfwCreateWindow(10, 10, "dog", nullptr, nullptr);
-    if (!window)
-        return std::unexpected(Error<WindowError>{WindowError::WindowCreationFailed, "Failed to create GLFW window"});
+$<void, Error<WindowError>> Init();
+void Shutdown();
+$<WindowHandle, Error<WindowError>> create(const char* title);
+}; // namespace Graphion::Window
 
-    return window;
-}
-
-
-
-int main(void) {
-
-  fmt::print("Just the usaul stuff nothing new, I will just try to speedrun "
-             "the getting started in a clean way to refresh my memory"
-             "We will try to full finish OpenGL before moving To vulkan\n");
-
-  glfwInit();
-  defer(glfwTerminate());
+$<void, Error<WindowError>> Graphion::Window::Init() {
+  if (!glfwInit())
+    return std::unexpected(
+        Error<WindowError>{WindowError::InitializationFailed,
+                           "Failed to initialize Window sub system"});
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -35,6 +27,34 @@ int main(void) {
 #ifdef __APPLE__
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
+  return {};
+}
+
+void Graphion::Window::Shutdown() { glfwTerminate(); }
+
+$<Graphion::Window::WindowHandle, Error<WindowError>>
+Graphion::Window::create(const char* title) {
+  GLFWwindow *window = glfwCreateWindow(10, 10, title, nullptr, nullptr);
+  if (!window)
+    return std::unexpected(Error<WindowError>{WindowError::WindowCreationFailed,
+                                              "Failed to create a window"});
+
+  return Graphion::Window::WindowHandle(window, glfwDestroyWindow);
+}
+
+int main(void) {
+
+  fmt::print("Just the usaul stuff nothing new, I will just try to speedrun "
+             "the getting started in a clean way to refresh my memory"
+             "We will try to full finish OpenGL before moving To vulkan, i "
+             "hope this codebase looks like C++\n");
+
+  if (auto result = Graphion::Window::Init(); !result) {
+    auto err = result.error();
+    err.logError();
+    return -1;
+  }
+  defer(Graphion::Window::Shutdown());
 
   return 0;
 }
